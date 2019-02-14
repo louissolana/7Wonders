319d1a686bf413085b2d0d8b5e1c493decaeeaba
Julian Arnaud <julian.arnaud.z10@gmail.com>
2019-02-14 22:08:26 +0100

    3 commandes a implem pour le client: defausser, construire, jouer

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -20,4 +20,32 @@ public class Client {
 
         return sb.toString();
     }
+
+    /**
+     * Action to discard a card and gain 3 gold
+     * @return Json string containing the command DISCARD and the Card sacrificed
+     * eg: send ["command":"DISCARD","card":"card_name"]
+     *     receive ["command":"DISCARDED","gold":3]
+     */
+    public String discard() {
+        return "";
+    }
+
+    /**
+     * Action to build a stage of the wonder
+     * @return Json string containing the command BUILD, the Card sacrificed and the number of the stage
+     */
+    public String build() {
+
+        return "";
+    }
+
+    /**
+     * Action to play a Card and trigger it's effect
+     * @return Json String containing the command PLAY and the Card played
+     */
+    public String playCard() {
+
+        return "";
+    }
 }
