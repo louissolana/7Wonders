2ed4e872ca751271edc061935a0d6dc0f80e3e06
oliviergaudard <olivier.gaudard@gmail.com>
2019-02-15 16:29:52 +0100

    struct

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -5,10 +5,31 @@ import game.Player;
 
 import java.util.Scanner;
 
+import commun.Coup;
+import commun.Identification;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
 public class Client {
     private int id;
     private Player player;
 
+    Identification moi = new Identification("Michel B", 42);
+
+    Socket connexion;
+    int propositionCourante = 50;
+
+    // Objet de synchro
+    final Object attenteDéconnexion = new Object();
 
     public Client(int id, Player player) {
         this.id = id;
@@ -65,11 +86,119 @@ public class Client {
         String line = entry.nextLine();
         return null;
     }
-    
+
     public void printHand(){
         for(Card c: player.getHand()){
             System.out.println(c.toString());
         }
     }
 
+    public Client(String urlServeur) {
+
+        try {
+            connexion = IO.socket(urlServeur);
+
+            System.out.println("on s'abonne à la connection / déconnection ");
+            ;
+
+            connexion.on("connect", new Emitter.Listener() {
+                @Override
+                public void call(Object... objects) {
+                    System.out.println(" on est connecté ! et on s'identifie ");
+
+                    // on s'identifie
+                    JSONObject id = new JSONObject(moi);
+                    connexion.emit("identification", id);
+
+                }
+            });
+
+            connexion.on("disconnect", new Emitter.Listener() {
+                @Override
+                public void call(Object... objects) {
+                    System.out.println(" !! on est déconnecté !! ");
+                    connexion.disconnect();
+                    connexion.close();
+
+                    synchronized (attenteDéconnexion) {
+                        attenteDéconnexion.notify();
+                    }
+                }
+            });
+
+
+            // on recoit une question
+            connexion.on("question", new Emitter.Listener() {
+                @Override
+                public void call(Object... objects) {
+                    int pas = 1;
+                    System.out.println("on a reçu une question avec " + objects.length + " paramètre(s) ");
+                    if (objects.length > 0) {
+                        System.out.println("la réponse précédente était : " + objects[0]);
+
+                        boolean plusGrand = (Boolean) objects[0];
+                        // false, c'est plus petit... !! erreur... dans les commit d'avant
+
+                        if (plusGrand) pas = -1;
+                        else pas = +1;
+
+
+                        System.out.println(objects[1]);
+
+                        // conversion local en ArrayList, juste pour montrer
+                        JSONArray tab = (JSONArray) objects[1];
+                        ArrayList<Coup> coups = new ArrayList<Coup>();
+                        for (int i = 0; i < tab.length(); i++) {
+
+                            try {
+                                coups.add(new Coup(tab.getJSONObject(i).getInt("coup"), tab.getJSONObject(i).getBoolean("plusGrand")));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        System.out.println(coups);
+
+                    }
+                    propositionCourante += pas;
+                    System.out.println("on répond " + propositionCourante);
+                    connexion.emit("réponse", propositionCourante);
+                }
+            });
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void seConnecter() {
+        // on se connecte
+        connexion.connect();
+
+        System.out.println("en attente de déconnexion");
+        synchronized (attenteDéconnexion) {
+            try {
+                attenteDéconnexion.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.err.println("erreur dans l'attente");
+            }
+        }
+    }
+
+    public static final void main(String []args) {
+        try {
+            System.setOut(new PrintStream(System.out, true, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        Client client = new Client("http://127.0.0.1:10101");
+        client.seConnecter();
+
+
+
+        System.out.println("fin du main pour le client");
+
+    }
 }
