a9d33b96a2fffd659ec5fb9ccf9791265e396cdd
Julian Arnaud <julian.arnaud.z10@gmail.com>
2019-03-11 13:47:56 +0100

    generer cartes

---
+++ src/main/java/server/Generator.java
@@ -0,0 +1,18 @@
+package server;
+
+import game.Board;
+import game.Card;
+
+import java.util.List;
+
+public class Generator {
+    public Generator(){}
+
+    public List<Card> generateCards() {
+        return null;
+    }
+
+    public List<Board> generateBoards() {
+        return null;
+    }
+}
