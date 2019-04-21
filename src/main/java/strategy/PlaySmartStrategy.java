541014d8e59100190bf16f76857eb4861d55aeb0
oliviergaudard <olivier.gaudard@gmail.com>
2019-04-21 16:59:02 +0200

    PlaySmartStrategy based on ScientistMilitary

--- src/main/java/strategy/PlaySmartStrategy.java
+++ src/main/java/strategy/PlaySmartStrategy.java
@@ -1,10 +1,13 @@
 package strategy;
 
+import game.Board;
 import game.Card;
 import game.Type;
 import org.json.simple.JSONObject;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class PlaySmartStrategy extends Stragegy {
     private Type color1;
@@ -15,7 +18,60 @@ public class PlaySmartStrategy extends Stragegy {
         color2 = c2;
     }
 
-    public JSONObject action(List<Card> hand) {
-        return null;
+    public JSONObject action(List<Card> hand)
+    {
+        JSONObject res = new JSONObject();
+        List<Card> color1Cards = new ArrayList<Card>();
+        List<Card> color2Cards = new ArrayList<Card>();
+        Boolean color1Card = false;
+        Boolean color2Card = false;
+
+        for(Card oneCard : hand)
+        {
+            Type type = oneCard.getType();
+            if(type == color1)
+            {
+                color1Card = true;
+                color1Cards.add(oneCard);
+            }
+            else if(type == color2)
+            {
+                color2Card = true;
+                color2Cards.add(oneCard);
+            }
+        }
+
+        if(color1Card)
+        {
+            Random rand = new Random();
+            int nbAlea = rand.nextInt(color1Cards.size());
+            Card cardPlayed = color1Cards.get(nbAlea);
+            res.put("command", "PLAY");
+            res.put("card", cardPlayed.getName());
+            System.out.println(cardPlayed.getName() + " a ete joue");
+            hand.remove(cardPlayed);
+
+        }
+        else if(!color1Card && color2Card)
+        {
+            Random rand = new Random();
+            int nbAlea = rand.nextInt(color2Cards.size());
+            Card cardPlayed = color2Cards.get(nbAlea);
+            res.put("command", "PLAY");
+            res.put("card", cardPlayed.getName());
+            System.out.println(cardPlayed.getName() + " a ete joue");
+            hand.remove(cardPlayed);
+        }
+        else{
+            Random rand = new Random();
+            int nbAlea = rand.nextInt(hand.size());
+            Card sacrificed = hand.get(nbAlea);
+            res.put("command", "DISCARD");
+            res.put("card", sacrificed.getName());
+            System.out.println(sacrificed.getName() + " a ete defausse");
+            hand.remove(sacrificed);
+        }
+        return res;
+
     }
 }
